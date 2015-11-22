5cd1e821e8a40fa88071da65b1573ae3923eb14c
PhiBo <phibo@dinotools.org>
2015-11-22 11:18:09 +0100

    Add htmlmin to grunt default

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,5 +61,5 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['jshint', 'uglify']);
+    grunt.registerTask('default', ['jshint', 'uglify', 'htmlmin']);
 };
